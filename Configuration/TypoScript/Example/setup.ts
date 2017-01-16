e6321b7aee4aa4a0e4b44e23c6218dda3b4b3783
Benjamin Serfhos <benjaminserfhos@hotmail.com>
2017-01-16 22:09:08 +0100

    [BUGFIX] Use correct mapping for configuration

--- Configuration/TypoScript/Example/setup.ts
+++ Configuration/TypoScript/Example/setup.ts
@@ -1,23 +1,21 @@
 plugin.tx_solr.fluidRendering {
-    fluidRendering {
-        view {
-            templateRootPaths {
-                10 = EXT:my_template/Resources/Private/Templates/Solr
-            }
-
-            partialRootPaths {
-                10 = EXT:my_template/Resources/Private/Templates/Partials/Solr
-            }
-
-            layoutRootPaths {
-                10 = EXT:my_template/Resources/Private/Templates/Layouts/Solr
-            }
+    view {
+        templateRootPaths {
+            10 = EXT:my_template/Resources/Private/Templates/Solr
         }
 
-        settings {
-            pages {
-                homepage = 1
-            }
+        partialRootPaths {
+            10 = EXT:my_template/Resources/Private/Templates/Partials/Solr
+        }
+
+        layoutRootPaths {
+            10 = EXT:my_template/Resources/Private/Templates/Layouts/Solr
+        }
+    }
+
+    settings {
+        pages {
+            homepage = 1
         }
     }
 }
